1faebdb664dff84cd860375383b446638198a5df
SCFC-PRC <yzs20101016@163.com>
2024-07-31 14:50:15 +0800

    Site updated: 2024-07-31 14:50:15

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/07/30/hello-world/","2024/07/30/new/"];function toRandomPost(){
+var posts=["2024/07/30/hello-world/","2024/07/30/new/","2024/07/31/朋友居然被拷打了？！/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
